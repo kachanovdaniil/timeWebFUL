f799a24682107ab1af7c3afee4e2f1224379269e
unknown <you@example.comkachanov113@yandex.ru>
2022-08-11 15:43:53 +0300

    first commit with project base

---
+++ src/main/java/com/example/timeWeb/TimeWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.timeWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/timeWeb/TimeWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.timeWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
